e2366b0e3526ab813c6eec01a15d91d515f5538d
Mika Braginsky <mikabr@stanford.edu>
2015-05-10 22:04:33 -0700

    updated data files and check unilemma script

--- check_unilemmas.R
+++ check_unilemmas.R
@@ -15,9 +15,13 @@ nor.wg <- nor.wg[,1:(ncol(nor.wg)-1)] %>%
 cro.wg <- read_csv('[Croatian_WG].csv') %>%
   mutate(language = "Croatian")
 
+spa.wg <-  read_excel('[Spanish_WG].xlsx')
+spa.wg <- spa.wg[,1:(ncol(spa.wg)-1)] %>%
+  mutate(language = "Spanish")
 
-diff <- bind_rows(anti_join(nor.wg,cro.wg, by = "uni_lemma"),
-                  anti_join(cro.wg,nor.wg, by = "uni_lemma")) %>%
+
+diff <- bind_rows(anti_join(spa.wg,eng.wg, by = "uni_lemma"),
+                  anti_join(eng.wg,spa.wg, by = "uni_lemma")) %>%
   select(language,uni_lemma,definition,category) %>%
   arrange(uni_lemma)
 
