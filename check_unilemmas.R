d9a1874e4c2eb4bb196aaca759e46883933a7a0c
Dan Yurovsky <dyurovsky@gmail.com>
2015-05-13 10:15:51 -0700

    added turkish

--- check_unilemmas.R
+++ check_unilemmas.R
@@ -3,6 +3,7 @@ library(tidyr)
 library(readxl)
 library(magrittr)
 library(readr)
+
 eng.wg <- read_excel('[English_WG].xlsx') %>%
   filter(!is.na(uni_lemma)) %>%
   mutate(language = "English")
@@ -19,9 +20,12 @@ spa.wg <-  read_excel('[Spanish_WG].xlsx')
 spa.wg <- spa.wg[,1:(ncol(spa.wg)-1)] %>%
   mutate(language = "Spanish")
 
+tur.wg <- read_csv('[Turkish_WG].csv') %>%
+  mutate(language = "Turkish")
 
-diff <- bind_rows(anti_join(spa.wg,eng.wg, by = "uni_lemma"),
-                  anti_join(eng.wg,spa.wg, by = "uni_lemma")) %>%
+
+diff <- bind_rows(anti_join(tur.wg,eng.wg, by = "uni_lemma"),
+                  anti_join(eng.wg,tur.wg, by = "uni_lemma")) %>%
   select(language,uni_lemma,definition,category) %>%
   arrange(uni_lemma)
 
